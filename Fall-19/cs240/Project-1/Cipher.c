30ee31d8a5ecb3747f662fcd00db6529e4b12e05
Vaastav Arora <arora74@purdue.edu>
2019-08-27 19:30:11 -0400

    cs240 update

--- Fall-19/cs240/Project-1/Cipher.c
+++ Fall-19/cs240/Project-1/Cipher.c
@@ -1,10 +1,61 @@
 #include <stdio.h>
 
-int decrypt() {
+int key = ~0;
+
+void  decrypt() {
+	
 }
 
-int encrypt() {
+void encrypt(int charCount ) {
+	
+	int keyTemp;
+	int counter = 0;
+	char c;
+	while ( charCount >= 0 ) {
+		c = getchar();
+		if ( c == ' ' || c == '\t' || c == '\n' ) {
+			puchar(c);
+		} else {
+			if ( counter % 4 == 0 ) {
+				keyTemp = 0;
+			} else {
+				keyTemp >>= 4;
+			}
+			char temp = (char) (15 & keyTemp);
+			c ^= temp;
+			putchar(c);
+			counter++;
+		}
+		charCount--;
+	}
+
 }
 
+int findKey() {
+	
+	int keyE,keyD;
+	scanf("%d %d\n",keyD,keyE);
+	return keyE ^ keyD;
+
+}
 int main() {
+	printf("Welcome to Cipher\n");
+
+	while(1) {
+		printf("Choose an option:\nA Encrpyt Text\nB Decrypt Text\nC Find Key\nD Exit\n);
+		char ans = getchar();
+		if( ans == 'D' ){
+			break;
+		} else if( ans == 'A' ) {
+			int charCount;
+			scanf("%d\n",charCount);
+			encrypt(charCount);
+		} else if( ans == 'B' ) {
+			decrypt();
+		} else if( ans == 'C ) {
+			findKey();
+		} else {
+			printf("Invalid Input\n");
+		}
+	}
 }
