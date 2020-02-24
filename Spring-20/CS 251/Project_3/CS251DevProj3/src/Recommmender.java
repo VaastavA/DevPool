e4f41c1eba197cbcff27a0e09d73157188a52711
chung170@purdue.edu <chung170@purdue.edu>
2020-02-24 18:21:54 -0500

    Update Recommmender.java

--- Spring-20/CS 251/Project_3/CS251DevProj3/src/Recommmender.java
+++ Spring-20/CS 251/Project_3/CS251DevProj3/src/Recommmender.java
@@ -45,15 +45,29 @@ public class Recommmender{
      * Do not change above code
      ********************************/
 
-
+    /**
+     * This function is for the calculate inversion counts of each option's.
+     * @param dataset is file name of all data for hash table
+     * @param options is the list of product name which we want to getting the inversion counts
+     * @return it is integer array of each option's inversion counts. The order of return should be matched with options.
+     */
+    public int[] inversionCounts(String dataset, String[] options) {
+    	// TODO
+        
+        return null;    	
+    }
+    
     /**
      * Get the sequence of recommendation from the dataset by sorting the inverse count.
      * Compare the similarity of depRating between RecentPurchase's and each option's.  
      * Use inverse count to get the similarity of two array. 
      * */
     public int[] recommend(String dataset, String recentPurchase, String[] options){
-
+    	// Don't change products values. 
+        products = options;
+    	// TODO
+    	
         return null;
-    }
+    
 
 }
